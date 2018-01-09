8f6ab3c24362bbaee3061f17bb8d7d5a222e2a31
Julie Workman <jworkman@calpoly.edu>
2018-01-09 13:05:34 -0800

    Add starting files.

---
+++ part1/SimpleLoop.java
@@ -0,0 +1,13 @@
+class SimpleLoop
+{
+   public static int sum(int low, int high)
+   {
+      /* TO DO:  Return the sum of the integers between
+         low and high (inclusive).  Yes, this can be
+         done without a loop, but the point is to
+         practice the syntax for a loop.
+      */
+
+      return 0;
+   }
+}
