3b271103da5d41cafe0a4be14ce494924b7af52e
rrohanramani <35237774+rrohanramani@users.noreply.github.com>
2018-01-17 22:10:23 -0800

    Updated

--- part1/SimpleArray.java
+++ part1/SimpleArray.java
@@ -2,20 +2,15 @@ class SimpleArray
 {
    public static int [] squareAll(int values[])
    {
-      /* TO DO: This size is not right.  Fix it to work with any
-         input array.  The length of an array is accessible through
-         an array's length field (e.g., values.length).
-      */
-      int [] newValues = new int[1];  // This allocates an array of integers.
-
-      /* TO DO: The output array, newValues, should hold as
-         its elements the square of the corresponding element
-         in the input array.
-
-         Write a loop to compute the square of each element from the
-         input array and to place the result into the output array.
-      */
-
+      int [] newValues = new int[values.length];
+      for (int i = 0; i < values.length; i++) {
+    	  newValues[i] =  (int) Math.pow(values[i], 2);
+      }
       return newValues;
+   }
+   
+   public static void main (String [] args) {
+	   int [] a = {1,2,3,4,5,6,7,8,9};
+	   System.out.println(squareAll(a));
    }
 }
