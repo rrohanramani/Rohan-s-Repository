b1d24b418752c17025d1b86b0632161b2d55933e
rrohanramani <35237774+rrohanramani@users.noreply.github.com>
2018-01-17 22:08:03 -0800

    Update BetterLoop.java

--- part1/BetterLoop.java
+++ part1/BetterLoop.java
@@ -1,11 +1,12 @@
 class BetterLoop
 {
-   public static boolean contains(int [] values, int v)
-   {
-      /* TO DO: if value v is in the array, return true.
-         If not, return false.  Use a "foreach" loop.
-      */
-
-      return true;  // A bit optimistic, but a real boolean value.
-   }
+   public static boolean contains(int [] values, int v) {
+      int count = 0;
+	  for (int element : values) {
+		if (element == v) {
+			count++;
+		}
+	  }
+      return (count > 0);  
+   }	
 }
