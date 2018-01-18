ef2a272ef565083eb3016854c64439c74f0892b9
rrohanramani <35237774+rrohanramani@users.noreply.github.com>
2018-01-17 22:11:03 -0800

    Updated

--- part1/SimpleIf.java
+++ part1/SimpleIf.java
@@ -1,11 +1,12 @@
 class SimpleIf
 {
-   public static double max(double x, double y)
-   {
-      /* TO DO: Write an if statement to determine which
-         argument is larger and return that value.
-      */
-
-      return 0; // clearly not correct -- but testable
-   }
+   public static double max(double x, double y) {
+		double bigNum = 0;
+		if (x >= y) {
+			bigNum = x;
+		} else {
+			bigNum = y;
+		}
+		return bigNum;
+	}
 }
