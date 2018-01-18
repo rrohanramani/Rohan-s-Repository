ac9abf62a9c685a1358d01ae7eae70abf9d65f82
rrohanramani <35237774+rrohanramani@users.noreply.github.com>
2018-01-17 22:14:21 -0800

    Updated

--- part1/ExampleMap.java
+++ part1/ExampleMap.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-class ExampleMap {
+class ExampleMap { 
    public static List<String> highEnrollmentStudents(Map<String, List<Course>> courseListsByStudentName, int unitThreshold) {
         List<String> overEnrolledStudents = new LinkedList<>();
         for (String x : courseListsByStudentName.keySet()) {
